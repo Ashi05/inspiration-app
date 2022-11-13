8a472de169dbb7b24d4bb0c41664b8671787d360
ashikum001 <104054702+ashikum001@users.noreply.github.com>
2022-11-13 22:45:35 +0530

    recieving data from weather api, created weather slice

--- src/api/weather/index.js
+++ src/api/weather/index.js
@@ -1,13 +1,18 @@
-import fetch from "node-fetch";
-
 const API_URL = `https://api.openweathermap.org/data/2.5/weather?appid=a11f388d6c658cf542564e2c61dd0fac
 &units=imperial`;
 
-const getWeather = async (city, state) => {
+export const getWeather = async (city, state) => {
   const response = await fetch(`${API_URL}&q=${city},${state}`);
   const json = await response.json();
 
-  return console.log(json);
+  return {
+    weatherMetadata: json.weather[0],
+    temperature: json.main.temp,
+  };
 };
 
-getWeather();
+const openWeatherMapApi = {
+  getWeather,
+};
+
+export default openWeatherMapApi;
--- src/features/weather/WeatherSlice.js
+++ src/features/weather/WeatherSlice.js
@@ -1,1 +1,34 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import openWeatherMapApi from "../../api/weather";
+
+export const getWeather = createAsyncThunk(
+  "weather/getWeather",
+  ({ city, state }) => openWeatherMapApi.getWeather(city, state)
+);
+
+export const weatherSlice = createSlice({
+  name: "weather",
+  initialState: {
+    city: "Seattle",
+    state: "Washington",
+    metadata: {},
+    temperature: "",
+  },
+  reducers: {
+    setCity: (state, action) => {
+      state.city = action.payload;
+    },
+    setState: (state, action) => {
+      state.state = action.payload;
+    },
+  },
+  extraReducers: {
+    [getWeather.fulfilled]: (state, action) => {
+      state.metadata = action.payload.weatherMetadata;
+      state.temperature = action.payload.temperature;
+    },
+  },
+});
+
+export const { setCity, setState } = weatherSlice.actions;
+export default weatherSlice.reducer;
